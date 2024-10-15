6afd03a66a998cd24b9cf91967292697e55b1819
D4C <zavazocnikovmaks@gmail.com>
2024-10-16 00:48:36 +1000

    empty

---
+++ dev_env/furnitureSite/furnitureSite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for furnitureSite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furnitureSite.settings')
+
+application = get_wsgi_application()
