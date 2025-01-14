d74c866f556973ee241e9a8d584dc7854a9ebc28
D4C <zavazocnikovmaks@gmail.com>
2025-01-15 08:23:31 +1000

    backend
    backend для работы с frontend

---
+++ furniture_company/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FurnitureList from './FurnitureList';
+
+const App = () => {
+    return (
+        <div className="App">
+            <FurnitureList />
+        </div>
+    );
+};
+
+export default App;
