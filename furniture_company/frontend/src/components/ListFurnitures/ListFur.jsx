ced8c055174c6f6d129034ccf0c9131337269869
D4C <146915116+D4CzMie@users.noreply.github.com>
2025-01-23 14:18:47 +1000

    Update ListFur.jsx

--- furniture_company/frontend/src/components/ListFurnitures/ListFur.jsx
+++ furniture_company/frontend/src/components/ListFurnitures/ListFur.jsx
@@ -1,23 +1,29 @@
 import styles from './ListFur.module.css';
+import {Navbar} from "../Navbar/index.js";
 import {useEffect, useState} from "react";
 
 export const ListFurnitures = () => {
     const [todos, setTodos] = useState([])
     useEffect(() => {
         const getTodos = async () => {
-            const todos = await fetch('http://127.0.0.1:8000/api/furniture/').then(res => res.json()).then(res => res)
+            const todos = await fetch('https://zmie.pythonanywhere.com/api/furniture/').then(res => res.json()).then(res => res)
             setTodos(todos)
         }
         getTodos()
     }, []);
 
-    return <div>
-        {todos.map(todo => (<div className={styles.menu}>
-            <img src={`${todo.image}`} alt={'мебель'} className={styles.image}/>
-            <span>{todo.name}</span>
-            <span>{todo.description}</span>
-            <div></div>
-        </div>))}
-    </div>
-
+    return (
+        <>
+            <Navbar/>
+            <div>
+                {todos.map(todo => (
+                    <div key={todo.name} className={styles.menu}>
+                        <img src={`${todo.image}`} alt={'мебель'} className={styles.image}/>
+                        <span>{todo.name}</span>
+                        <span>{todo.description}</span>
+                    </div>
+                ))}
+            </div>
+        </>    
+    )
 }
